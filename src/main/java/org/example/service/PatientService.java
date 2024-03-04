f8b1a6faf845e916f90cb9d36978eeb2f6db88fe
janath <janathhma@gmail.com>
2024-03-04 21:15:51 +0530

    define service layer

---
+++ src/main/java/org/example/service/PatientService.java
@@ -0,0 +1,7 @@
+package org.example.service;
+
+import org.example.dto.PatientDto;
+
+public interface PatientService {
+    public PatientDto addPatient(PatientDto barrowerDto);
+}
---
+++ src/main/java/org/example/service/impl/PatientServiceImpl.java
@@ -0,0 +1,34 @@
+package org.example.service.impl;
+import lombok.RequiredArgsConstructor;
+import org.example.dto.PatientDto;
+import org.example.entity.Patient;
+import org.example.repository.PatientRepository;
+import org.example.service.PatientService;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PatientServiceImpl implements PatientService {
+
+    final PatientRepository patientRepository;
+    ModelMapper modelMapper;
+    @Bean
+    public void setUp(){
+        this.modelMapper = new ModelMapper();
+    }
+    @Override
+    public PatientDto addPatient(PatientDto patientDto) {
+        Patient patient = modelMapper.map(patientDto, Patient.class);
+        if(patient != null){
+            Patient patient1 = patientRepository.save(patient);
+            String userid = patient1.generatepatientId();
+            patient1.setPatientId(userid);
+            patientRepository.save(patient1);
+            PatientDto patientDto1 = modelMapper.map(patient1,PatientDto.class);
+            return patientDto1;
+        }
+        return null;
+    }
+}
