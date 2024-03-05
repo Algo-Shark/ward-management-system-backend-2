d9583a2170c1d767726880f3ce8906537738f8c5
janath <janathhma@gmail.com>
2024-03-05 05:52:01 +0530

    Define Ward entity

---
+++ src/main/java/org/example/entity/Ward.java
@@ -0,0 +1,22 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Ward {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String bedId;
+    private int capacity;
+    private String wardType;
+}
