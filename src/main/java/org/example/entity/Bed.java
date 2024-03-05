b144231c48415a39c9d1876c0e50b7775f0277ed
janath <janathhma@gmail.com>
2024-03-05 06:09:45 +0530

    Define bed entity

---
+++ src/main/java/org/example/entity/Bed.java
@@ -0,0 +1,19 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Bed {
+    @Id
+    private Long id;
+    private String bed_id;
+    private String status;
+
+}
