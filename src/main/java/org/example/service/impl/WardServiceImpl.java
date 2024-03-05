53b83b6e95f0c6dec7333f375952d96d171d564c
janath <janathhma@gmail.com>
2024-03-05 06:55:02 +0530

    define addWard method inside service layer

---
+++ src/main/java/org/example/service/impl/WardServiceImpl.java
@@ -0,0 +1,31 @@
+package org.example.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dto.WardDto;
+import org.example.entity.Ward;
+import org.example.repository.WardRepository;
+import org.example.service.WardService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WardServiceImpl implements WardService {
+    final WardRepository wardRepository;
+    ModelMapper modelMapper;
+    public void setUp(){
+        this.modelMapper = new ModelMapper();
+    }
+    @Override
+    public WardDto addWard(WardDto wardDto) {
+        Ward ward = modelMapper.map(wardDto,Ward.class);
+        if(ward!=null){
+            Ward ward1 = wardRepository.save(ward);
+            ward1.setWardId(ward.generateWardId());
+            wardRepository.save(ward1);
+            WardDto wardDto1 = modelMapper.map(ward1,WardDto.class);
+            return wardDto1;
+        }
+        return null;
+    }
+}
