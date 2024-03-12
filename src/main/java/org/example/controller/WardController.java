148122b71f5aa6429c8a67682440ba7226ecdff4
janath <janathhma@gmail.com>
2024-03-13 05:02:01 +0530

    Complete Beds are automatically allocated when a ward is created, according to the ward's capacity

--- src/main/java/org/example/controller/WardController.java
+++ src/main/java/org/example/controller/WardController.java
@@ -1,29 +1,28 @@
 package org.example.controller;
-
 import org.example.dto.WardDto;
 import org.example.dto.responseDto.CustomResponse;
+import org.example.dto.responseDto.WardResponse;
 import org.example.service.WardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequestMapping("/ward")
 @CrossOrigin
 public class WardController {
     @Autowired
     WardService wardService;
-
     @PostMapping("/add")
-    public ResponseEntity<CustomResponse<String>> addWard(@RequestBody WardDto wardDto){
+    public ResponseEntity<WardResponse<String>> addWard(@RequestBody WardDto wardDto){
         WardDto wardDto1 = wardService.addWard(wardDto);
         if(wardDto1!=null){
-            CustomResponse<String> customResponse = new CustomResponse<>(wardDto1.getWardId(), "Success");
-            return new ResponseEntity<>(customResponse, HttpStatus.OK);
+            WardResponse<String> wardResponse = new WardResponse<>(wardDto1.getWardId(), wardDto1.getBedList());
+            System.out.println(wardDto1.getBedList().size());
+            return new ResponseEntity<>(wardResponse, HttpStatus.OK);
         }else {
-            CustomResponse<String> customResponse = new CustomResponse<>("Alredy registerd","Unsuccessful");
-            return new ResponseEntity<>(customResponse, HttpStatus.BAD_REQUEST);
+            WardResponse<String> wardResponse = new WardResponse<>("Alredy registerd");
+            return new ResponseEntity<>(wardResponse, HttpStatus.BAD_REQUEST);
         }
     }
     @GetMapping("/get")
--- src/main/java/org/example/service/impl/WardServiceImpl.java
+++ src/main/java/org/example/service/impl/WardServiceImpl.java
@@ -1,33 +1,54 @@
 package org.example.service.impl;
 import lombok.RequiredArgsConstructor;
+import org.example.dto.BedDto;
 import org.example.dto.WardDto;
+import org.example.entity.Bed;
 import org.example.entity.Ward;
+import org.example.enums.BedStatus;
+import org.example.repository.BedRepository;
 import org.example.repository.WardRepository;
+import org.example.service.BedService;
 import org.example.service.WardService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.Lifecycle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class WardServiceImpl implements WardService {
     final WardRepository wardRepository;
+    @Autowired
+    BedService bedService;
+
     ModelMapper modelMapper;
+    @Autowired
+    BedRepository bedRepository;
     @Bean
     public void set(){
         this.modelMapper = new ModelMapper();
     }
     @Override
     public WardDto addWard(WardDto wardDto) {
-
-        Ward ward = new Ward(wardDto.getWardId(),wardDto.getCapacity(),wardDto.getWardType());
-        if(ward!=null){
-            Ward ward1 = wardRepository.save(ward);
-            WardDto wardDto1 = modelMapper.map(ward1,WardDto.class);
-            return wardDto1;
+        Ward ward = new Ward(wardDto.getWardId(), wardDto.getCapacity(), wardDto.getWardType());
+        Ward savedWard = wardRepository.save(ward);// Save the ward
+        List<BedDto> bedList = new ArrayList<>();
+        int length = ward.getCapacity();
+        for (int i = 0; i < length; i++) {
+            Bed bed = new Bed(bedService.generateBedId(), BedStatus.VACANT);
+            bedList.add(new BedDto(bed.getBedId(),bed.getStatus()));
+            Bed savedBed = bedRepository.save(bed);
+            savedBed.setWard(savedWard);
+            savedWard.getBedList().add(bed);
         }
-        return null;
+        wardRepository.save(savedWard);
+        return new WardDto(ward.getWardId(), ward.getCapacity(), ward.getWardType(), bedList);
     }
+
 
     @Override
     public String generateWardId() {
